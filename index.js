cb0c4fa8192aa76ab3f76ecf7013a0ac44ffbf6f
Usman Khan <uk03777@gmail.com>
2021-10-16 09:24:43 +0500

    Added JavaScript Files

---
+++ index.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('.wrapper'),
+inputPart = wrapper.querySelector('.input-part'),
+infoTxt = inputPart.querySelector('.info-txt'),
+inputField = inputPart.querySelector('input');
+
+inputField.addEventListener("keyup", e=> {
+
+    // if user pressed enter btn and input value in not empty
+    if (e.key == "Enter" && inputField.value !== "") {
+        requestApi(inputField.value);
+    }
+});
+
+function requestApi(city) {
+    console.log(city);
+}
